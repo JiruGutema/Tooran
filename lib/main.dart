2e47220c29de2b26039f47015b10c6ab8433ff14
JiruGutema <jethior1@gmail.com>
2025-03-13 10:21:24 +0300

    Linux Version 1

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'help_page.dart'; // Import Help page
 import 'contact_page.dart';
 import 'home_page.dart';
 import 'about.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await windowManager.ensureInitialized();
+
+  // Set initial window size
+  windowManager.setSize(const Size(500, 600));
+
+  // Prevent resizing (optional)
+  windowManager.setResizable(false);
+
   runApp(ToDoApp());
 }
 
@@ -21,7 +32,7 @@ class ToDoApp extends StatelessWidget {
         '/': (context) => ToDoHomePage(),
         '/help': (context) => HelpPage(),
         '/contact': (context) => ContactPage(),
-        '/about':(context) => About(),
+        '/about': (context) => About(),
       },
     );
   }
